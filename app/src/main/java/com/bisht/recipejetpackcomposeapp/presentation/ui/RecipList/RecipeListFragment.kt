3e14826dd08769319eb43ccedf6d798d0120b7b1
bisht2961 <19bcs2961@gmail.com>
2021-08-19 21:32:58 +0530

    RecipeJetPackComposeApp

--- app/src/main/java/com/bisht/recipejetpackcomposeapp/presentation/ui/RecipList/RecipeListFragment.kt
+++ app/src/main/java/com/bisht/recipejetpackcomposeapp/presentation/ui/RecipList/RecipeListFragment.kt
@@ -5,29 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -36,7 +23,6 @@ import com.bisht.recipejetpackcomposeapp.presentation.Components.*
 import com.bisht.recipejetpackcomposeapp.presentation.theme.AppTheme
 import com.bisht.recipejetpackcomposeapp.utils.RECIPE_IMAGE_HEIGHT
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -44,6 +30,8 @@ class RecipeListFragment: Fragment() {
 
     @Inject
     lateinit var application: BaseApplication
+
+    private val snackbarController = SnackbarController(lifecycleScope)
 
     private val viewModel: RecipeListViewModel by viewModels()
 
@@ -71,14 +59,12 @@ class RecipeListFragment: Fragment() {
                                 onQueryChanged = viewModel::onQueryChanged,
                                 onExecuteSearch = {
                                     if(viewModel.selectedCategory.value?.value == "milk"){
-                                        lifecycleScope.launch {
-                                            scaffoldState
-                                                .snackbarHostState
-                                                .showSnackbar(
-                                                    message = "Invalids Category: Milk!",
-                                                    actionLabel = "Hide"
-                                                )
-                                        }
+                                        snackbarController
+                                            .showSnackbar(
+                                                scaffoldState = scaffoldState,
+                                                message = "Invalids Category: Milk!",
+                                                actionLabel = "Hide"
+                                            )
                                     }else{
                                         viewModel.newSearch()
                                     }
